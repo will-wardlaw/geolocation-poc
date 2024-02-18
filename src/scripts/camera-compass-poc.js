7a1d2521771755defba9a749a82e2545a94b237b
Will Wardlaw <william.wardlaw@gmail.com>
2024-02-17 18:52:31 -0700

    Display direction info if available

--- src/scripts/camera-compass-poc.js
+++ src/scripts/camera-compass-poc.js
@@ -1,9 +1,28 @@
 const videoElement = document.querySelector('#camera-container');
+const noDirectionIndicator = document.querySelector('#noDirection');
+const directionInfo = document.querySelector('#directionInfo');
 
 const constraints = {
     audio: false,
     video: { facingMode: "environment" }
 }
+
+const displayObj = (obj, el) => {
+    el.innerHTML = '';
+    
+    for(let prop in obj) {
+        const strong = document.createElement('strong');
+        strong.textContent = `${prop}: `;
+
+        const span = document.createElement('span');
+        span.textContent = obj[prop];
+
+        const para = document.createElement('p');
+        para.append(strong, span);
+        el.append(para);
+    }
+};
+
 
 async function attachCameraToVideoElement(constraints, videoElement) {
     
@@ -20,3 +39,14 @@ async function attachCameraToVideoElement(constraints, videoElement) {
 }
 
 attachCameraToVideoElement(constraints, videoElement);
+
+window.addEventListener("deviceorientationabsolute", (event) => {
+    console.log(event);
+
+    noDirectionIndicator.setAttribute('class', 'hidden');
+    directionInfo.setAttribute('class', 'visible');
+
+    const orientationInfo = (({ alpha, beta, gamma, absolute }) => ({ alpha, beta, gamma, absolute }))(event);
+
+    displayObj(orientationInfo, directionInfo);
+});
