c5bf30817b46d8b6be4ad0b43deea73979f67501
will-wardlaw <william.wardlaw@gmail.com>
2026-05-27 00:26:11 -0600

    Correct transform (#20)
    * Spell "transform" correctly
    
    * Rotate in opposite direction
    
    * Stop trying to force the orientation

--- src/scripts/camera-compass-poc.js
+++ src/scripts/camera-compass-poc.js
@@ -31,7 +31,7 @@ async function attachCameraToVideoElement(constraints, videoElement) {
         videoElement.srcObject = stream;
         var angle = screen.orientation.angle;
         if(angle != 0) {
-            videoElement.style.transorm = `rotate(${angle}deg)`;
+            videoElement.style.transform = `rotate(-${angle}deg)`;
         }
         
         videoElement.onloadedmetadata = () => {
@@ -172,7 +172,8 @@ function renderOrientation(event) {
     displayObj(orientationInfo, directionInfo);
 }
 
-window.screen.orientation.lock('landscape-primary');
+// This doesn't work. In the future, try requesting fullscreen access first.
+//window.screen.orientation.lock('landscape-primary');
 
 attachCameraToVideoElement(constraints, videoElement);
 
